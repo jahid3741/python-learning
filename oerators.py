32f381b01ea16d1e1cc7184f41a5e5a9a53f532f
jahid3741 <mdj868445@gmail.com>
2026-07-18 12:09:49 +0600

    add identity logical operation

---
+++ oerators.py
@@ -0,0 +1,37 @@
+x  = 15
+y = 24
+sum = x + y
+print("The sum of x and y is:", sum)
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
+x = 12
+y = 5
+
+print(x // y)
+# tenary operator
+num = 6
+
+x = "Fri" if num == 5 else "Sat" if num == 6 else "Sun" if num == 7 else "weekday"
+
+print(x)
+
+
+# identity operator
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
